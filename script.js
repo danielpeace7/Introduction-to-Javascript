1ba35b9cfaed6b5ce24bee67db62abb955228b75
Olayanju Daniel <102909682+danielpeace7@users.noreply.github.com>
2022-06-21 20:01:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = 'OLAYANJU DANIEL';
+
+alert(`My name is ${name}`);
+
+const height = '1.75M';
+
+alert(`I'm ${height} tall`);
+
+const country = 'NIGERIA';
+
+alert(`I'm from ${country}`);
